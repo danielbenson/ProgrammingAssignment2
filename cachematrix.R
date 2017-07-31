9fdc1e9ebde36734b478dea610a15da4471c5f7f
Daniel Benson <30567390+danielbenson@users.noreply.github.com>
2017-07-31 16:35:23 -0500

    Update cachematrix.R
    Added comments for reference.

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,21 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The goal of this assignment is to use the "<<-" Special Assignment 
+## (aka "superassignment") Operator to demonstrate the use of "closures" or
+## functions written by another function.
+## Some excellent examples of how to approach their use can be found in
+## Hadley Wickham's open-source textbook "Advanced R" in the Functional
+## Programming section (http://adv-r.had.co.nz/). As is often the case,
+## CRAN has us covered, too 
+## (https://cran.r-project.org/doc/manuals/R-intro.html#Scope).
+## There are some useful R packages that could simplify the process of
+## caching and inverting matrices, like SOAR by Bill Venables
+## (https://cran.r-project.org/web/packages/SOAR/index.html), memoise
+## (https://cran.r-project.org/web/packages/memoise/index.html), and 
+## R.cache (https://cran.rstudio.com/web/packages/R.cache/index.html),
+## but those shortcuts don't teach us about "<<-" and may be overkill for
+## our needs here.
 
-## Write a short comment describing this function
+## First, we'll define the "makeCacheMatrix" function to create a special 
+## "matrix" object that can cache its inverse.
 
   makeCacheMatrix <- function(x = matrix()) {
     inv <- NULL
@@ -18,8 +32,10 @@
          getInverse = getInverse)
 }
 
-
-## Write a short comment describing this function
+## Next, we'll define the "cacheSolve" function to compute the inverse of the 
+## special "matrix" returned by makeCacheMatrix above. If the inverse has  
+## already been calculated (and the matrix has not changed), then the  
+## cacheSolve should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
     ## Return a matrix that is the inverse of 'x'
@@ -33,3 +49,7 @@ cacheSolve <- function(x, ...) {
     x$setInverse(inv)
     inv
 }
+
+## Now, we've successfully "cached" the matrix inversion task with "<<-", 
+## which modifies existing variables in parent environments rather than
+## assigning in the current environment.
